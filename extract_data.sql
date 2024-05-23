636f9b135081fed70439a528bac082a61a6c152a
Aleksa Toroman <aleksa.toroman@asseco-see.rs>
2024-05-23 21:06:22 +0200

    Begin working on documentation

--- extract_data.sql
+++ extract_data.sql
@@ -1,3 +1,5 @@
+
+-- https://skyserver.sdss.org/dr18/SearchTools/sql
 SELECT TOP 500000
    p.objid,p.ra,p.dec,p.u,p.g,p.r,p.i,p.z,
    p.run, p.rerun, p.camcol, p.field,
@@ -8,3 +10,5 @@ SELECT TOP 500000
 FROM PhotoObj AS p
    JOIN SpecObj AS s ON s.bestobjid = p.objid
 WHERE s.class IN ('STAR', 'GALAXY')
+
+
