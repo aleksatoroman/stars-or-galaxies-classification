0911a3efded787ca19a789d4e938dc2c555db712
Aleksa Toroman <aleksa.toroman@asseco-see.rs>
2024-05-28 22:39:57 +0200

    Fix sql

--- extract_data.sql
+++ extract_data.sql
@@ -3,8 +3,6 @@
 SELECT TOP 500000
    p.objid,p.ra,p.dec,p.u,p.g,p.r,p.i,p.z,
    p.run, p.rerun, p.camcol, p.field,
-   p.modelFlux_u, p.modelFlux_g,p.modelFlux_r,
-   p.modelFlux_i,p.modelFlux_z,
    s.specobjid, s.class, s.z as redshift,
    s.plate, s.mjd, s.fiberid
 FROM PhotoObj AS p
